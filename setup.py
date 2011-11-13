52f796cf684392c4190e2857be0bbb0e5d1ecc5b
fredrik <none@none>
2011-11-13 22:10:55 +0100

    UTF-8 woes

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
-# -*- encoding: utf-8 -*- 
+# -*- coding: utf-8 -*- 
 from setuptools import setup
 
-def read(fname):
-    """Nice idea stolen from example project"""
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "hgapi",
@@ -13,11 +9,24 @@ setup(
     test_suite = "testhgapi.TestHgAPI",
 
     # metadata
-    author = "Fredrik Håård",
+    author = u"Fredrik Håård",
     author_email = "fredrik@haard.se",
     description = "Python API to Mercurial using the command-line interface",
     license = "MIT or BSD or WTF",
     keywords = "mercurial api",
     url = "https://bitbucket.org/haard/hgapi",   # project home page, if any
-    long_description = read("README.rst")
+    long_description = """
+hgapi is a pure-Python API to Mercurial, that uses the command-line
+interface instead of the internal Mercurial API. The rationale for
+this is twofold: the internal API is unstable, and it is GPL.
+
+hgapi works for Mercurial < 1.9, and will instantly reflect any
+changes to the repositiry, unlike interfaces based on the
+CommandServer (http://mercurial.selenic.com/wiki/CommandServer). It
+also has a really permissive license (do whatever you want, don't
+blame me).
+
+For example of code that uses this API, take a look at
+https://bitbucket.org/haard/autohook which now uses hgapi
+exclusively."""
 )
