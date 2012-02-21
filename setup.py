a110d4901342b8634a62fc65574e341bcdb724f7
fredrik <none@none>
2012-02-21 22:06:44 +0100

    modified setup.py to gitapi

--- setup.py
+++ setup.py
@@ -3,28 +3,19 @@ from setuptools import setup
 
 
 setup(
-    name = "hgapi",
-    version = "1.1.0",
-    packages = ['hgapi'],
-    test_suite = "hgapi.testhgapi.TestHgAPI",
+    name = "gitapi",
+    version = "0.0.1",
+    packages = ['gitapi'],
+    test_suite = "gitapi.testgitapi.TestGitAPI",
     author = "Fredrik Håård",
     author_email = "fredrik@haard.se",
-    description = "Python API to Mercurial using the command-line interface",
+    description = "Python API to Git using the command-line interface",
     license = "Do whatever you want, don't blame me",
-    keywords = "mercurial api",
-    url = "https://bitbucket.org/haard/hgapi",   # project home page, if any
+    keywords = "git api",
+    url = "https://bitbucket.org/haard/gitapi",   # project home page, if any
     long_description = """
-hgapi is a pure-Python API to Mercurial, that uses the command-line
-interface instead of the internal Mercurial API. The rationale for
-this is twofold: the internal API is unstable, and it is GPL.
-
-hgapi works for Mercurial < 1.9, and will instantly reflect any
-changes to the repository, unlike interfaces based on the
-CommandServer (http://mercurial.selenic.com/wiki/CommandServer). It
-also has a really permissive license (do whatever you want, don't
-blame me).
-
-For example of code that uses this API, take a look at
-https://bitbucket.org/haard/autohook which now uses hgapi
-exclusively."""
+hgapi is a pure-Python API to Git, that uses the command-line
+interface. This is a fork of https://bitbucket.org/haard/ with the 
+intention of making a unified API for a subset of git and hg 
+functionality possible"""
 )
