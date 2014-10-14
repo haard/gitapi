22e3027f8969be3bf63202938325523eae4ebf46
fredrik <none@none>
2014-10-14 08:51:44 +0100

    1.0.1a2

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
 
 setup(
     name = "gitapi",
-    version = "1.0.1a1",
+    version = "1.0.1a2",
     packages = ['gitapi'],
     test_suite = "gitapi.testgitapi.TestGitAPI",
     author = "Fredrik Håård",
@@ -15,7 +15,7 @@ setup(
     url = "https://bitbucket.org/haard/gitapi",   # project home page, if any
     long_description = """
 gitapi is a pure-Python API to Git, that uses the command-line
-interface. This is a fork of https://bitbucket.org/haard/ with the 
-intention of making a unified API for a subset of git and hg 
+interface. This is a fork of https://bitbucket.org/haard/ with the
+intention of making a unified API for a subset of git and hg
 functionality possible"""
 )
